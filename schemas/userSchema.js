8410da4740571be075ab3e856858e105bc56cb1a
Collins Sirmah <csirmah@gmail.com>
2015-07-25 21:42:43 -0400

    Uploaded a skeleton. Not tested yet

---
+++ schemas/userSchema.js
@@ -0,0 +1,34 @@
+var mongoose = require('mongoose')
+var Schema   = mongoose.Schema()
+
+
+//User Schema to create the models from 
+//TODO: Make sure that the type is accurate
+var userSchema = new Schema({
+	first_name: String,
+	last_name:  String,
+	email: String,
+	password: {type: String, required: true},
+	is_staff: Boolean,
+	is_superuser: Boolean,
+	is_active: Boolean,
+	last_login: Date,
+	date_joined: Date, 
+})
+
+
+// We need methods which will interact with the data e.g
+
+// Before save if this function is run 
+userSchema.pre('save', function(next){
+
+	if(!date_joined)
+		this.date_joined = new Date()
+	
+	next();
+})
+// We will need a method which will hash the password before storage
+var User = mongoose.model('User', userSchema)
+
+module.exports = User;
+
