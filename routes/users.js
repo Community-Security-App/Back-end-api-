8d87c685edc71cf61073014c1ab13deefe355e4a
Collins Sirmah <csirmah@gmail.com>
2015-07-28 03:06:49 -0400

    added more requests to the users

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
+// all the user requests will be located here
 var express = require('express');
 var router  = express.Router();
 var user    = require('../schemas/userSchema')
 
-/* GET users listing. */
+/* GET all the users */
 router.get('/', function(req, res, next) {
   user.find(function(err, users) {
             if(err)
@@ -13,4 +14,47 @@ router.get('/', function(req, res, next) {
         })
 })
 
+/* Returns a user with a certain id */
+router.get('/:uid', function(req, res, next) {
+	user.find(req.param.uid, function(err, user) {
+		if (err)
+			res.send(err)
+
+		res.json(user)
+	})
+})
+
+/* Creates a new user */
+router.post('/', function(req, res, next) {
+
+	var newUser = new user({
+		first_name: req.body.first_name,
+		last_name:  req.body.last_name,
+		email: req.body.email,
+		password: req.body.password,
+		is_staff: req.body.is_staff,
+		is_superuser: req.body.is_superuser,
+		is_active: req.body.isactive,
+		last_login: req.body.last_login,
+		date_joined: req.body.date_joined
+	})
+
+	newUser.save(function(err){
+		if (err) 
+			res.send(err)
+
+		res.json({"success": "1"})
+	})
+})
+
+//TODO: PUT command. 
+router.delete('/:uid', function(req, res, next) {
+    user.findByIdAndRemove(req.param.uid, function(err){
+        if (err)
+            res.send(err)
+
+        res.json({"success": "1"})
+    })
+
+})
 module.exports = router;
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// This is where all the server handling logic will be located
+
 var server = require('./app.js')
 
 server.listen(3000)
