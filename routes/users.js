fb36804177b6d52deeafa6859ed66317b7400f0c
Collins Sirmah <csirmah@gmail.com>
2015-08-02 21:23:26 -0400

    Rebased the user

--- routes/users.js
+++ routes/users.js
@@ -1,37 +1,62 @@
-// all the user requests will be located here
-var express = require('express');
-var router  = express.Router();
+var user    = require('../schemas/userSchema');
 
+//Posts a user
+exports.postUsers = function(req, res) {
 
+     newUser = new user({
+        first_name: req.body.first_name,
+        last_name:  req.body.last_name,
+        email: req.body.email,
+        password: req.password,
+        is_staff: req.is_staff,
+        is_superuser: req.is_superuser,
+        is_active: req.is_active,
+        last_login: Date,
+        date_joined: Date
+    });
 
-/* returns all the users */
-router.get('/', function(req, res, next) {
-  user.find(function(err, users) {
+     newUser.create(req.body, function (err, post) {
+        if(err)
+            res.json(err)
+    
+        res.json(body);
+    
+    }
+  });
+
+};
+
+//Gets all the users
+exports.getUsers = function(req, res) {
+    user.find(function(err, users) {
             if(err)
                 res.send(err)
 
             res.json(users)
 
         })
-})
+};
 
+//A certain user ID
+exports.getUserId = function(req, res) {
+    user.find(req.params.id, function(err, user) {
+        if (err)
+            res.send(err)
 
-/* Returns a user with a certain id */
-router.get('/id', function(req, res, next) {
-	user.find(req.params.id, function(err, user) {
-        console.log(req.params.id)
-		if (err)
-			res.send(err)
+        res.json(user)
+    })
 
-		res.json(user)
-	})
-})
+};
 
+//changes a user
+exports.putUser = function(req, res) {
 
-/* Creates a new user */
-router.post('/', function(req, res, next) {
+};
 
-    newUser = new user({
+//deletes a user
+exports.deleteUser = function( req, res) {
+    
+     newUser = new user({
         first_name: req.body.first_name,
         last_name:  req.body.last_name,
         email: req.body.email,
@@ -52,26 +77,5 @@ router.post('/', function(req, res, next) {
     
     }
   });
-});
 
-//Updates a certain user in the database
-router.put('/:id', function(req, res, next){
-     user.findByIdAndUpdate(req.params.id, req.body, function (err, post){
-        if (err)
-            res.send(err)
-        else
-            res.send(post)
-    })
-}) 
-
-// Deletes a particular item from the dbase
-router.delete('/:id', function(req, res, next) {
-    user.findByIdAndRemove(req.params.id, function(err, post){
-        if (err)
-            res.send(err)
-
-        res.json(post)
-    })
-
-})
-module.exports = router;
+};
