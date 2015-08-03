9df705e97b96c26677cc207666461d20370a54b9
Collins Sirmah <csirmah@gmail.com>
2015-08-02 20:18:03 -0400

    Added eventSchema

--- routes/events.js
+++ routes/events.js
@@ -1,1 +1,20 @@
-@author : Collins Sirmah
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema
+
+var eventSchema = new Schema({
+    description = String, 
+    threatLevel = enum, 
+    location    = String,
+    latitude    = float,
+    longitude   = float,
+    occurenceTime = Date,
+    reportedTime  = Date,
+    //reported_by (FK, User)
+    upvoteCount   = int,
+    downvoteCount = int,
+    flagCount     = int
+    deleted       = boolean,
+    flagged       = boolean
+}); 
+
+module.exports = eventSchema;
