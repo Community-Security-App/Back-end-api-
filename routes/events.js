1e4b68b751e274f750fabe2f91c7c40d2617cc9e
Collins Sirmah <csirmah@gmail.com>
2015-08-02 20:35:54 -0400

    Created endpoints for GET and POST for all events

---
+++ routes/events.js
@@ -0,0 +1,44 @@
+var uEvent = require('../schemas/eventSchema');
+
+
+//Endpoint for all the get
+
+exports.postEvents = function(req, res) {
+
+    var newEvent = new uEvent();
+
+    newEvent.description = req.body.description,
+    newEvent.threatLevel = req.body.threatLevel, 
+    newEvent.location    = req.body.location,
+    newEvent.latitude    = req.body.latitude,
+    newEvent.longitude   = req.body.longitude,
+    newEvent.occurenceTime = req.body.occurenceTime,
+    newEvent.reportedTime  = req.body,reportedTime,
+    //reported_by (FK, User)
+    newEvent.upvoteCount   = req.body.upvoteCount,
+    newEvent.downvoteCount = req.body.downvoteCount,
+    newEvent.flagCount     = req.body.flagCount
+    newEvent.deleted       = req.body.deleted,
+    newEnvent.flagged       = req.body.flagged
+
+    newEvent.save(function(err) {
+        if (err)
+            res.send(err);
+
+        res.json({"success" : 1});
+
+    });
+};
+
+// End point for all the GET 
+exports.getEvents = function(req, res) {
+
+    uEvent.find(function (err, uevent) {
+        if(err)
+            res.send(err);
+
+        res.json(uevent);
+    });
+};
+
+//Endpoint for GET 
