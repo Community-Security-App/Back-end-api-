78b094e17ad9f88daeedfff6faaf6c2e83cf8104
Collins Sirmah <csirmah@gmail.com>
2015-08-04 13:36:44 -0400

    Delete works

--- routes/users.js
+++ routes/users.js
@@ -54,27 +54,11 @@ exports.putUser = function(req, res) {
 
 //deletes a user
 exports.deleteUser = function( req, res) {
-    
-     newUser = new user({
-        first_name: req.body.first_name,
-        last_name:  req.body.last_name,
-        email: req.body.email,
-        password: req.password,
-        is_staff: req.is_staff,
-        is_superuser: req.is_superuser,
-        is_active: req.is_active,
-        last_login: Date,
-        date_joined: Date
-    });
+        user.findByIdAndRemove(req.params.id, function(err, user) {
+            if(err)
+                res.send(err)
 
-     newUser.create(req.body, function (err, post) {
-
-        if(err)
-            res.json(err)
-    
-        res.json(body);
-    
-    
-  });
+            res.json({"Sucess": "User removed"})
+        })
 
 };
