5d5708b78bddaed3bb504b4c5b843fc5808928d0
Collins Sirmah <csirmah@gmail.com>
2015-08-08 22:01:25 -0400

    Added Cclient Schema for Auth2

---
+++ schemas/client.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose')
+
+var clientSchema = new mongoose.Schema({
+	name: {
+		type: String, 
+		unique: true,
+		require: true
+	},
+	id: {
+		type: String,
+		require: true
+	},
+	secret: {
+		type: String,
+		require: true
+	},
+	userId: {
+		type: String, 
+		require: true
+	}
+})
+
+module.exports = mongoose.model('Client', clientSchema)
+
