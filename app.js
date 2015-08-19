db35d766dd4e3e09e22dd22ce61d8bb1c7f5d472
Collins Sirmah <csirmah@gmail.com>
2015-08-19 15:45:12 -0400

    Basic cleaning of the code

--- app.js
+++ app.js
@@ -25,20 +25,17 @@ var app = express();
 
 //Dbase connection 
 mongoose.connect('mongodb://localhost/testDB');
-//var db = mongoose.connection;
+
 mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
 
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'jade');
-//app.engine('jade', engines.jade)
+
 app.engine('ejs', engines.ejs)
 //TODO: Remove this 
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
@@ -55,7 +52,6 @@ app.use(session({
 
 
 //Path to the user routes
-//app.use('/users');
 
 var router = express.Router();
 
--- routes/oauth2.js
+++ routes/oauth2.js
@@ -9,22 +9,14 @@ var uid = require('rand-token').uid
 var server = oauth2orize.createServer();
 
 
-//serilize function
 server.serializeClient(function(client, callback) {
-	//TODO: Remove this soonest possible 
-	//console.log(client)
-	console.log("This is serialize")
 	return callback(null, client._id)
 })
 
-//Deserialize function 
+
 server.deserializeClient(function(id, callback) {
 	Client.findOne({_id: id}, function (err, client) {
-		if (err) {return callback(err); }
-		//TODO: Make sure to remove this
-		//console.log("Found a client in auth dese")
-		//console.log(client)
-		console.log("This is deserialize")
+		if (err) { return callback(err); }
 		return callback(null, client);
 
 	});
@@ -32,19 +24,16 @@ server.deserializeClient(function(id, callback) {
 
 server.grant(oauth2orize.grant.code(function(client, redirectUri, user, ares, callback) {
 
+	//TODO: Implement this more elegantly
 	var newCode = new Code();
 		
 		newCode.value = uid(16);
 		newCode.clientId = client._id; 
 		newCode.redirectUri =redirectUri;
 		newCode.userId = user._id;
-		console.log(newCode)
-		console.log("Was the new code ever generated?");
 
 	newCode.save(function(err) {
 		if (err) {return callback(err); }
-		console.log(newCode)
-		console.log("This is the new code which has been generated"); 
 		callback(null, newCode.value);
 	});
 }));
@@ -52,17 +41,12 @@ server.grant(oauth2orize.grant.code(function(client, redirectUri, user, ares, ca
 server.exchange(oauth2orize.exchange.code(function(client, code, redirectUri, callback) {
 	Code.findOne({value: code}, function (err, authCode) {
 		
-		//TODO: Make sure to remove this
-		console.log(authCode);
-		console.log("This is the authocode")
 
 		if (err) { return callback(err); }
 		if (authCode == undefined) { return callback(null, false); }
 		if (client._id.toString() !== authCode.clientId) {
-			console.log("Got suck in to string")
 			return callback(null, false); }
-		if (redirectUri != authCode.redirectUri) {
-			console.log("Got stuck in the redirect URI")
+		if (redirectUri != authCode.redirectUri) { 
 			return callback(null, false); }
 
 		authCode.remove(function (err) {
@@ -76,9 +60,6 @@ server.exchange(oauth2orize.exchange.code(function(client, code, redirectUri, ca
 
 			newToken.save(function (err) {
 				if (err) {return callback(err); }
-
-				console.log("Token has been returned with the callback")
-				console.log(newToken)
 				callback(null, newToken);
 			});
 		});
@@ -89,14 +70,10 @@ exports.authorization = [
 	server.authorization(function(clientId, redirectUri, callback) {
 		Client.findOne({id: clientId}, function (err, client) {
 			if (err) {return callback(err); }
-			//TODO: remove this 
-			//console.log("This is where we export authorization")
-			//console.log(client)
 			return callback(null, client, redirectUri);
 		});
 	}),
 	function(req, res) {
-		console.log({transactionID: req.oauth2.transactionID, user:req.user, client:req.oauth2.client})
 		res.render('dialog', {transactionID: req.oauth2.transactionID, user:req.user, client:req.oauth2.client});
 	}]
 
