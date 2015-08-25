3e1b28a62068cfaa72345111e2c17f55e07a0fb7
Collins Sirmah <csirmah@gmail.com>
2015-08-25 19:39:10 -0400

    Few comments added

--- routes/comments.js
+++ routes/comments.js
@@ -1,1 +1,14 @@
 var comment = require('../schemas/commentSchema')
+
+
+//Post comments to a certain post
+exports.postComments = function(req, res){
+
+}
+
+//Get comments tagged to a certain post
+exports.getComment = function(req, res){
+
+}
+
+//Get a comment by a certain ID
