d6c7b8bdf0bd7804aa8db38c2a14ac89604cf096
ziban <csirmah@gmail.com>
2015-08-30 23:04:15 -0400

    Update eventAction.js

--- routes/eventAction.js
+++ routes/eventAction.js
@@ -2,20 +2,3 @@ var eventAction = require('../schemas/eventActionSchema')
 
 
 
-void myFunction(int x) {
-
-	if (x <= 0){
-
-		System.out.println("We have gotten to the base of the function");
-	}
-
-	else {
-		myFunction(x - 1);
-	}
-
-}
-
-
-var myFunction = function(int x){
-
-}
