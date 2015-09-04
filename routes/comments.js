8ef8d3cf89266d65a2cfe2f93599dd05257f7866
Collins Sirmah <csirmah@gmail.com>
2015-09-03 23:00:09 -0400

    More endpoints def

--- routes/comments.js
+++ routes/comments.js
@@ -2,16 +2,24 @@ var comment = require('../schemas/commentSchema')
 
 
 //Post comments to a certain post
-exports.postComments = function(req, res){
+exports.postComments = function(req, res) {
 
 }
 
 //Get comments tagged to a certain post
-exports.getComment = function(req, res){
+exports.getComment = function(req, res) {
 
 }
 
 //Get a comment by a certain ID
-exports.getCommentByID = function(req, res){
+exports.getCommentByID = function(req, res) {
   
+}
+
+exports.flagComment = function(req, res) {
+
+}
+
+exports.deleteComment = function(req, res) {
+	
 }
